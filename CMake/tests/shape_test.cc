2005fc98b179d7f55574cb74b65558efca17a73e
정필규 <pilkyu1@hancomwith.com>
2022-01-06 09:48:46 +0900

    - 테스트 케이스 추가

--- CMake/tests/shape_test.cc
+++ CMake/tests/shape_test.cc
@@ -8,9 +8,19 @@
 namespace shape {
 namespace {
 
-TEST(ShapeTest, RectangleSize) {
+TEST(ShapeTest1, RectangleSize_true) {
   Rectangle rect(10, 2);
   EXPECT_EQ(rect.GetSize(), 20);
+}
+
+TEST(ShapeTest1, RectangleSize_false) {
+  Rectangle rect(10, 2);
+  EXPECT_EQ(rect.GetSize(), 10);
+}
+
+TEST(ShapeTest2, RectangleSize_false) {
+  Rectangle rect(10, 2);
+  EXPECT_EQ(rect.GetSize(), 10);
 }
 
 }  // namespace
